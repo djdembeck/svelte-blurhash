321ed2f28b378261965289ef793c6a76f2b063e4
h416 <h416@users.noreply.github.com>
2022-09-03 08:17:50 +0900

    fix example image url

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -17,7 +17,7 @@
         </BlurhashPicture>
 
         <BlurhashImage
-                src={"https://blurha.sh/assets/images/img1.jpg"}
+                src={"https://blurha.sh/12c2aca29ea896a628be.jpg"}
                 hash={"LEHV6nWB2yk8pyoJadR*.7kCMdnj"}
                 width={320}
                 height={206}
@@ -25,21 +25,21 @@
         />
 
         <BlurhashImage
-                src={"https://blurha.sh/assets/images/img2.jpg"}
+                src={"https://blurha.sh/e08adedc9f82ade9f9d5.jpg"}
                 hash={"LGF5]+Yk^6#M@-5c,1J5@[or[Q6."}
                 width={320}
                 height={206}
         />
 
         <BlurhashImage
-                src={"https://blurha.sh/assets/images/img3.jpg"}
+                src={"https://blurha.sh/2af1b155f29d7089e3d9.jpg"}
                 hash={"L6Pj0^i_.AyE_3t7t7R**0o#DgR4"}
                 width={320}
                 height={206}
         />
 
         <BlurhashImage
-                src={"https://blurha.sh/assets/images/img4.jpg"}
+                src={"https://blurha.sh/ea9e499f8080ce9956a8.jpg"}
                 hash={"LKO2?U%2Tw=w]~RBVZRi};RPxuwH"}
                 width={320}
                 height={206}
